10efd75d2728ca7a8d9d8ff1b81e2de8980fa69a
Amber Roberts <a.roberts_90@yahoo.com>
2024-11-28 13:36:53 -0600

    added files with vegatables

---
+++ data/vegetables.js
@@ -0,0 +1,24 @@
+const vegetables = [
+    {
+        name: 'corn',
+        color: 'yellow',
+        readyToEat: true
+    },
+    {
+        name: 'zucchini',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name: 'potato',
+        color: 'brown',
+        readyToEat: false
+    },
+    {
+        name: 'tomato',
+        color: 'red',
+        readyToEat: true
+    }
+]
+
+module.exports = vegetables;
---
+++ views/Demo.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+class Demo extends React.Component {
+    render () {
+        return (
+            <>
+                <h1>This is a demonstration</h1>
+                <p> And then I can add some things</p>
+                <img src="https://ih1.redbubble.net/image.5528417048.5616/raf,360x360,075,t,fafafa:ca443f4786.u1.jpg"  />
+            </>
+        )
+    }
+}
+
+module.exports = Demo;
